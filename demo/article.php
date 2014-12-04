f7cb036da2be6844fcd1b83ecf1f136438a1ee2e
meepie12013 <meepie12013@gmail.com>
2014-12-04 15:07:58 -0500

    New

---
+++ demo/article.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Design Observer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Open+Sans:300' rel='stylesheet' type='text/css'>
+        <script src="jquery.js"></script> 
+        
+    </head>
+    
+    <body>
+        
+        <header role="banner" class="fixed">
+            <?php include("header.html");?>
+        </header>
+        
+        <main role="main" id="content" style="background-color:#fff;border-width: 0 40px 0 40px;border-color: #f4d267;border-style: solid;box-sizing: border-box;"> 
+            <section class="fixed-clear" style="margin-top:0;padding-top:0;">
+                <a id="grid"></a>
+                
+                   <div class="row">
+                   </div>
+                
+            </section>
+            
+            <section>
+                <a id="css3"></a>
+                
+                <div class="row" style="margin-top:20px;">
+                    <div class="col-2 vcenter equal-height">
+                        <center><h1 style="margin:0;padding:0;">You're reading</h1><br></center>
+                        <a href="article.html"><img src="../img/Untitled-1.jpg" width="100%" height="auto">
+                        <hr>
+                     <a href="/"><img src="../img/Untitled-6.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-2.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-3.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-4.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-5.jpg" width="100%" height="auto"></a><br><br>
+                        <a href="/"><img src="../img/Untitled-1.png" width="100%" height="auto"></a>
+                    </div>
+                    <div class="col-8 vcenter equal-height">
+                        
+                        
+<h1 style="font-size:4rem;">Adrian Shaughnessy</h1>
+<h2>An Interview with Neville Brody</h2>
+                        <img src="../img/likelove.jpg" width="200px;"><br>
+                        <div><em>As part of a week of posts celebrating the GraphicsRCA: Fifty Years exhibition, Adrian Shaughnessy interviews the RCA&rsquo;s Dean of the School of Communication and designer Neville Brody.<br/><br/>&nbsp;</em></div>
+<div><strong>Adrian Shaughnessy</strong>: In your view, what are the unique and defining characteristics of the <a href="http://www.rca.ac.uk/">RCA</a>?</div>
+<div>&nbsp;</div>
+<div><strong>Neville Brody</strong>: The RCA can be defined by its resistance to being defined. It operates as a laboratory for developing skilled, dangerous minds&ndash;minds that will lead and change society and industry. It is also highly collaborative, based on a principle of developing cross-disciplinary practice. Many students work across departments and skills, graduating into a world that increasingly demands such lateral capability and thought. The RCA is small, has a teaching ratio of 15:1, is solely postgraduate, and at over 175 years, is the oldest in continual existence art and design school in the world.</div>
+<div>&nbsp;</div>
+<div><strong>AS</strong>: Since the 1980s you have been at the forefront of graphic design practice, yet up until arriving at the RCA five years ago, you had no teaching experience. Today you are <a href="http://www.rca.ac.uk/more/staff/professor-neville-brody/">Dean of the School of Communication</a>, what do you bring to the role that someone with more experience in education might not bring?</div>
+<div>&nbsp;</div>
+<div><strong>NB</strong>: What I lack in academic experience, I hope I make up for in vision and strategic thinking. Whilst needing to comply with a large amount of administrative deliverables and meetings, I spend as much time as I can looking at ways of moving into new and relevant spaces.&nbsp;Teaching in an elective titled Design Without, which operates as a kind of de-programming boot camp, allows me to explore, invent, and seek out new mechanisms to extend to the wider curriculum, as well as performing the vital role of helping me keep in touch with students.</div>
+<div>&nbsp;</div>
+<div><strong>AS</strong>: In your role as Dean you are involved in developing and growing a <a href="http://www.rca.ac.uk/research-innovation/">research culture at the College</a>&ndash;can you say something about this in light of the fact that for many graphic designers, research (in the academic sense) and practice are not always seen as compatible?</div>
+<div>&nbsp;</div>
+<div><strong>NB</strong>: Research belongs at the heart of both graphic design and the educational process, and we embrace it as a way to see, inform and change the world. Graphic design, in its role as observer and reflector, necessarily incorporates and operates from a contextual and critical position. Graphic design, as with all forms of communication, is persuasive. How it is done, why it is done, who it is done for, and for what outcome, are all intrinsic questions, and as such necessitate both a critical and self-critical approach.</div>
+<div>&nbsp;</div>
+<div>The research culture at the College, especially through collaborative projects like CX (<a href="http://thecreativeexchange.org/">The Creative Exchange</a>), sets out to explore social design through innovative practice. The conflict at the heart of graphic design between that of persuasion and enablement continues to demand such mechanisms.</div>
+<div>&nbsp;</div>
+<div><strong>AS</strong>: We are currently looking back at <a href="http://graphics50.rca.ac.uk/#home">fifty years of graphic design</a> at the RCA. How would you characterize the past half-century at the RCA, and what is your vision for the future?</div>
+<div>&nbsp;</div>
+<div><strong>NB</strong>: What was recognized fifty years ago as a radical craft, perfectly placed to engineer change in society through conscious intervention, is once again how we are beginning to see graphic design, as we did in the seventies, eighties and at some point in the nineties. This speeding oscillation produces great individuals and ideas at the turning of the circle, and throws them off, often at wild tangents. Graphic design is always informed and enabled by technology, to which it has a strong affinity and relationship. As a means for making and distributing change, so graphic design morphs again. There will always be a relevant role and function for graphic design, especially at the RCA, and the skills necessary that underpin its industry potentially stand as both a voice of reason and reasoning, and a mechanism for change, which is the only constant I can imagine for its future.</div>
+<div>&nbsp;</div>
+<div><strong>AS</strong>: How has the experience of immersing yourself in academia changed you as an individual and as a practitioner?&nbsp;</div>
+<div>&nbsp;</div>
+<div><strong>NB</strong>: My confidence has certainly grown in the area of academia, honed by the enormous pressure felt by everyone involved in education today. The Higher Education landscape particularly has become virtually unrecognizable through the disruption caused by unjust and debilitating higher fees, a dramatically shifting reductive academic criteria based on an uninformed view of relevance (<a href="http://www.ed.gov/stem">STEM</a> anyone?), increased numbers, financial insecurity, the stress of greater staff performance accountability, and a new emphasis on industry partnerships.</div>
+<div>&nbsp;</div>
+<div>In many ways it has helped me mature as a practitioner. I appreciate a wider landscape and have gained a deeper understanding, but equally, I yearn for the opposite and I&rsquo;m realising again how important it is to disobey. I sense the need for radical, unprecious and disruptive design again, and feel myself being drawn anew towards unstructured chance and structured mayhem.</div>
+<div></div> <br/><br/>
+                        <hr><small>
+<span style="color:black">Adrian Shaughnessy is a graphic designer and writer based in London. He is a senior tutor in Visual Communication at the Royal College of Art and a founding partner in <a href="http://www.uniteditions.com" target="_blank"><span class="s1">Unit Editions</span></a> a publishing company producing books on design and visual culture. <em><a href="http://www.uniteditions.com/shop/essays-scratching-the-surface" target="_blank">Scratching the Surface</a></em>, a collection of his journalism, has recently been published.</span>
+<br/><br/></small>
+
+
+                        
+                    </div>
+                    <div class="col-2 vcenter equal-height" style="border-style:solid;border-color:#9e9e9e;border-width:1px;padding:10px;box-sizing:border-box;"><center><img src="../img/openhouse.png" width="100%;"><br><br>
+                        <textarea style="width:90%;height:1.5rem;">Search</textarea></center><br>
+                        
+                        <a href="/"><b>Home</b></a><br/>
+<br/>
+<a href="/archive.php" class="strong">Archive</a> | 
+<a href="/topics.php" class="strong">Topics</a> | 
+<a href="/jobs/" class="strong">Jobs</a> | 
+<a href="http://places.designobserver.com" target="_blank" class="strong">Places</a><br>
+<a href="/books.php" class="strong">Books</a> | 
+<a href="/5050.php" class="strong">50|50</a> | 
+<a href="/login.php" class="strong">Profile</a>
+<br/><br>
+<a href="/contributors.php"><b>Contributors</b></a><br/><br/>
+<span class="writers">
+<a href="/profile/michaelbierut/110">Michael Bierut</a><br/>
+<a href="/profile/johnfoster/275">John Foster</a><br/>
+<a href="/profile/jessicahelfand/45">Jessica Helfand</a><br/>
+<a href="/profile/adamharrisonlevy/61">Adam H. Levy</a><br/>
+<a href="/profile/debbiemillman/69">Debbie Millman</a><br/>
+<a href="/profile/bonniesiegler/406">Bonnie Siegler</a><br/>
+<a href="/profile/robwalker/274">Rob Walker</a><br/>
+<br/>
+<em>Berlin</em><br/>
+<a href="/profile/erikspiekermann/437">Erik Spiekermann</a><br/>
+<br/>
+<em>London</em><br/>
+<a href="/profile/rickpoynor/81">Rick Poynor</a><br/>
+<a href="/profile/adrianshaughnessy/89">Adrian Shaughnessy</a><br/>
+<br/>
+<em>Paris</em><br/>
+<a href="/profile/veroniquevienne/428">V&eacute;ronique Vienne</a><br/>
+<br/>
+</span>
+                    
+                    </div>
+                            </div>
+                            <!-- extra stuff -->
+                            
+            </section>
+        </main>
+
+        <footer style="color:#757575;background-color:#f4d267;border-top-style:solid;border-top-color:#757575;border-top-width:1px;">
+            <?php include("footer.html");?>
+        </footer>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
+        <script src="js/plugins.min.js"></script>
+        <script src="js/main.min.js"></script>
+               
+    </body>
+    
+</html>
---
+++ demo/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Design Observer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Open+Sans:300' rel='stylesheet' type='text/css'>
+        <script src="jquery.js"></script> 
+        
+    </head>
+    
+    <body>
+        
+        <header role="banner" class="fixed">
+            <?php include("header.html");?>
+        </header>
+        
+        <main role="main" id="content" style="background-color:#fff;border-width: 0 40px 0 40px;border-color: #f4d267;border-style: solid;box-sizing: border-box;"> 
+            <section class="fixed-clear" style="margin-top:0;padding-top:0;">
+                <a id="grid"></a>
+                
+                   <div class="row">
+                   </div>
+                
+            </section>
+            
+            <section>
+                <a id="css3"></a>
+                
+                <div class="row" style="margin-top:20px;">
+                    <div class="col-2 vcenter equal-height">
+                        <center><h1 style="margin:0;padding:0;">articles</h1><br></center>
+                     <a href="/"><img src="../img/Untitled-6.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-2.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-3.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-4.jpg" width="100%" height="auto"></a><br>
+                        <a href="/"><img src="../img/Untitled-5.jpg" width="100%" height="auto"></a><br><br>
+                        <a href="/"><img src="../img/Untitled-1.png" width="100%" height="auto"></a>
+                    </div>
+                    <div class="col-8 vcenter equal-height">
+                        <a href="article.php"><img src="../img/Untitled-1.jpg" width="100%" height="auto"></a><br>
+                        <hr/><h3><b>Opportunities</b></h3><table width="100%">
+                            <tr>
+                                <td width="35%">
+                                    <a href="/"><h4 style="margin:0;padding:0;">Fashion Designer</h4></a>
+<p style="margin:0;padding:0;">Location: Newark, NJ</p>
+<p style="margin:0;padding:0;">Posted: Dec 3 </p>
+                                    <br><a href="/"><h4 style="margin:0;padding:0;">Motion Designer</h4></a>
+<p style="margin:0;padding:0;">Location: Palo Alto, CA</p>
+<p style="margin:0;padding:0;">Posted: Dec 3 </p>
+                                    <br><a href="/"><h4 style="margin:0;padding:0;">Illustrator</h4></a>
+<p style="margin:0;padding:0;">Location: Baltimore, MD</p>
+<p style="margin:0;padding:0;">Posted: Dec 3 </p>
+                                </td>
+                                <td width="35%">
+                                    <a href="/"><h4 style="margin:0;padding:0;">Fashion Designer</h4></a>
+<p style="margin:0;padding:0;">Location: Newark, NJ</p>
+<p style="margin:0;padding:0;">Posted: Dec 3 </p>
+                                    <br><a href="/"><h4 style="margin:0;padding:0;">Motion Designer</h4></a>
+<p style="margin:0;padding:0;">Location: Palo Alto, CA</p>
+<p style="margin:0;padding:0;">Posted: Dec 3 </p>
+                                    <br><a href="/"><h4 style="margin:0;padding:0;">Illustrator</h4></a>
+<p style="margin:0;padding:0;">Location: Baltimore, MD</p>
+<p style="margin:0;padding:0;">Posted: Dec 3 </p>
+                                </td>
+                                <td width="30%">
+                                    <img src="../img/log%20in.jpg" width="100%"><br>
+                                    <p><a href="/">Post a Job</a></p>
+                                    <p><a href="/">View all Jobs</a></p>
+                                
+
+
+                                </td>
+                            </tr>
+                        </table>
+                        
+                    </div>
+                    <div class="col-2 vcenter equal-height" style="border-style:solid;border-color:#9e9e9e;border-width:1px;padding:10px;box-sizing:border-box;"><center><img src="../img/openhouse.png" width="100%;"><br><br>
+                        <textarea style="width:90%;height:1.5rem;">Search</textarea></center><br>
+                        
+                        <a href="/"><b>Home</b></a><br/>
+<br/>
+<a href="/archive.php" class="strong">Archive</a> | 
+<a href="/topics.php" class="strong">Topics</a> | 
+<a href="/jobs/" class="strong">Jobs</a> | 
+<a href="http://places.designobserver.com" target="_blank" class="strong">Places</a><br>
+<a href="/books.php" class="strong">Books</a> | 
+<a href="/5050.php" class="strong">50|50</a> | 
+<a href="/login.php" class="strong">Profile</a>
+<br/><br>
+<a href="/contributors.php"><b>Contributors</b></a><br/><br/>
+<span class="writers">
+<a href="/profile/michaelbierut/110">Michael Bierut</a><br/>
+<a href="/profile/johnfoster/275">John Foster</a><br/>
+<a href="/profile/jessicahelfand/45">Jessica Helfand</a><br/>
+<a href="/profile/adamharrisonlevy/61">Adam H. Levy</a><br/>
+<a href="/profile/debbiemillman/69">Debbie Millman</a><br/>
+<a href="/profile/bonniesiegler/406">Bonnie Siegler</a><br/>
+<a href="/profile/robwalker/274">Rob Walker</a><br/>
+<br/>
+<em>Berlin</em><br/>
+<a href="/profile/erikspiekermann/437">Erik Spiekermann</a><br/>
+<br/>
+<em>London</em><br/>
+<a href="/profile/rickpoynor/81">Rick Poynor</a><br/>
+<a href="/profile/adrianshaughnessy/89">Adrian Shaughnessy</a><br/>
+<br/>
+<em>Paris</em><br/>
+<a href="/profile/veroniquevienne/428">V&eacute;ronique Vienne</a><br/>
+<br/>
+</span>
+                    
+                    </div>
+                            </div>
+                            <!-- extra stuff -->
+                            
+            </section>
+        </main>
+
+        <footer style="color:#757575;background-color:#f4d267;border-top-style:solid;border-top-color:#757575;border-top-width:1px;">
+            <?php include("footer.html");?>
+        </footer>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../js/vendor/jquery-2.1.1.min.js"><\/script>')</script>
+        <script src="js/plugins.min.js"></script>
+        <script src="js/main.min.js"></script>
+               
+    </body>
+    
+</html>
